5eea184a1829e66b3e4b0341f877d3b88cabca11
josedihego <josedihego@gmail.com>
2022-11-10 14:15:18 -0300

    com FTP servidor e FTP cliente

---
+++ A09_cliente_FTP.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+from ftplib import FTP
+
+# ftplib: implementação protocilo FTP cliente
+
+ftp = FTP('')
+# criação de um objeto FTP
+ftp.connect('10.25.120.246', 1026)
+# conexão ao servidor FTP
+ftp.login('patativa', 'p34f2')
+# login no servidor com usuário e senha
+ftp.cwd('/')
+
+
+# navegação no diretório do usuário
+
+def enviarArquivo():
+    nome_arquivo = input('Informe o nome do arquivo:')
+    ftp.storbinary('STOR ' + nome_arquivo, open(nome_arquivo, 'rb'))
+    # armazena arquivo remotamente no servidor FTP em modo binário de leitura
+
+
+def baixarArquivo():
+    nome_arquivo = input('Informe o nome do arquivo:')
+    arquivo_local = open(nome_arquivo, 'wb')
+    # abre arquivo em modo binário de escrita
+    ftp.retrbinary('RETR ' + nome_arquivo, arquivo_local.write, 1024)
+    arquivo_local.close()
+
+
+def criarDiretorio():
+    nome_diretorio = input('informe o nome do novo diretório: ')
+    ftp.mkd(nome_diretorio)
+
+
+def removerDiretorio():
+    nome_diretorio = input('informe o nome do diretório a ser removido: ')
+    ftp.rmd(nome_diretorio)
+
+
+def removerArquivo():
+    nome_arquivo = input('informe o nome do arquivo a ser removido: ')
+    ftp.delete(nome_arquivo)
+
+
+def renomearArquivo():
+    nome_atual = input('informe o nome atual do  arquivo: ')
+    nome_novo = input('informe o novo nome do  arquivo: ')
+    ftp.rename(nome_atual, nome_novo)
+
+
+opcao = 0
+while (opcao != 1):
+    print('Informe uma das opções')
+    print('\t 1. sair')
+    print('\t 2. listar arquivos')
+    print('\t 3. enviar arquivo')
+    print('\t 4. baixar arquivo')
+    print('\t 5. deletar arquivo')
+    print('\t 6. criar diretório')
+    print('\t 7. deletar diretório')
+    print('\t 8. renomear arquivo')
+    opcao = int(input())
+    if opcao == 1:
+        ftp.quit()
+    elif opcao == 2:
+        ftp.retrlines('LIST')
+    elif opcao == 3:
+        enviarArquivo()
+    elif opcao == 4:
+        baixarArquivo()
+    elif opcao == 5:
+        removerArquivo()
+    elif opcao == 6:
+        criarDiretorio()
+    elif opcao == 7:
+        removerDiretorio()
+    elif opcao == 8:
+        renomearArquivo()
---
+++ A09_servidor_FTP.py
@@ -0,0 +1,36 @@
+
+from pyftpdlib.authorizers import DummyAuthorizer
+# - DummyAuthorizer gerencia autenticações e permissões do servidor FTP
+# - senhas salvas sem qualquer proteção em arquivos de texto
+#  - serve como base para criação de gerenciadores de autorização mais complexos
+# - usado dentro de um ...
+from pyftpdlib.handlers import FTPHandler
+# - FTPHandler para verificar usuário e senha,
+# - acessar diretório de trabalho
+# - checar permissões de leitura e escrita
+from pyftpdlib.servers import FTPServer
+# - FTPServer: servidor FTP
+
+
+autoridade = DummyAuthorizer()
+autoridade.add_user(
+    'admin', 'admin', '/home/josedihego/servidorFTP/', perm='elradfmw')
+autoridade.add_user('patativa', 'p34f2', '/home/josedihego/servidorFTP/', perm='elradfmw')
+
+gerenteFTP = FTPHandler
+gerenteFTP.authorizer = autoridade
+
+servidor = FTPServer(('10.25.120.246', 1026), gerenteFTP)
+servidor.serve_forever()
+
+# permissões
+# "e" = mudar diretório (CWD, CDUP )
+# "l" = listar arquivos (LIST, NLST, STAT, MLSD, MLST, SIZE )
+# "r" = baixar arquivos (RETR)
+# "a" = adicionar dados a um arquivo existente (APPE )
+# "d" = remover um arquivo ou diretório (DELE, RMD)
+# "f" = renomear um arquivo ou diretório (RNFR, RNTO )
+# "m" = criar um diretório (MKD )
+# "w" = armazenar um arquivo no servidor (STOR, STOU )
+# "M" = mudar as permissões de um arquivo (SITE CHMOD )
+# "T" = mudar o tempo do arquivo (SITE MFMT )
