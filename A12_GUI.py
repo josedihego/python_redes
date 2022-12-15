7e361f159b7d478a6310c8ecbab84c712d0c2bff
José Dihego <josedihego@gmail.com>
2022-12-15 19:40:44 -0300

    Add files via upload

--- A12_GUI.py
+++ A12_GUI.py
@@ -4,7 +4,7 @@ tela.title('Empresa do José Carlos de Oliveira Santos')
 tela.geometry('700x400')
 frame_pr = tk.Frame(master=tela, width= 700, height=400,)
 fonte = ('Times',15)
-campo_responsavel = tk.Label(master=frame_pr, text = 'Responsavel', font=fonte, width=10, height=2)
+campo_responsavel = tk.Label(master=frame_pr, text = 'Responsavel', font=fonte, width=12, height=2)
 entrada_responsavel = tk.Entry(master=frame_pr, width=30, font=fonte)
 campo_responsavel.grid(row=0, column=0)
 entrada_responsavel.grid(row=0, column=1)
@@ -13,6 +13,12 @@ campo_descricao =  tk.Label(master=frame_pr, text='Descrição', font=fonte, wid
 entrada_descricao = tk.Entry(master=frame_pr, width=30, font=fonte)
 campo_descricao.grid(row=1, column=0)
 entrada_descricao.grid(row=1, column=1)
+
+campo_endereco = tk.Label(master= frame_pr, text= 'Endereço', font=fonte, width=10, height=2)
+entrada_endereco = tk.Entry(master=frame_pr, width=30, font = fonte)
+campo_endereco.grid(row=2, column=0)
+entrada_endereco.grid(row=2,column=1)
+
 # RESPONSÁVEL, DESCRIÇÃO, ENDEREÇO
 frame_pr.pack()
 tela.mainloop()
